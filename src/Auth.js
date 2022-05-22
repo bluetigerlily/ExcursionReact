721dcaf0484e67325ddea27638249b9138d2758e
Haley Moses <vhr825@gmail.com>
2022-05-22 02:46:12 -0500

    added netlify auth

--- src/Auth.js
+++ src/Auth.js
@@ -32,40 +32,5 @@ netlifyIdentity.refresh().then((jwt)=>console.log(jwt))
 netlifyIdentity.setLocale('en');
 
 
-const updateLoginText = ()=>{
-    document.getElementById("primary-text").textContent="Looks like you've logged in successfully! Now try clicking the 'Test Auth' link at the top!"
-}
-const compareDate = (exp) => {
-    const d = Date.now()
-    if (d > exp){
-    console.log("Your identity session has expired")
-    netlifyIdentity.logout();
-    netlifyIdentity.open();
-    } else {
-    console.log("the token hasn't expired, yet")
-    updateLoginText()
-    }
-}
-if (window.netlifyIdentity) {
-    netlifyIdentity.on("login", user => {
-    console.log(user)
-    compareDate(user.token.expires_at)
-    });
-    window.netlifyIdentity.on("init", user => {
-    if (!user) {
-        window.netlifyIdentity.on("login", () => {
-            updateLoginText()
-        });
-    }
-});   
-}
-
-
-if (window.netlifyIdentity){
-netlifyIdentity.on("init", user => console.log(user));
-netlifyIdentity.on("open", () => console.log("Widget opened"));
-netlifyIdentity.on("close", () => console.log("Widget closed"));
-}
-
 
 
