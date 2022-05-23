d3559f283c3321351b7c25b0f631f2a7621ecd90
Haley Moses <vhr825@gmail.com>
2022-05-23 08:15:50 -0500

    added React Router and routed the navbar

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,18 +5,7 @@ import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
 import IconButton from '@mui/material/IconButton';
 import StarBorderIcon from "@mui/icons-material/StarBorder";
-import Paper from '@mui/material/Paper';
-import { styled } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
-
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
 
 export default function Home() {
   
