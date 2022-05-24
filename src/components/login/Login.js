7078b048cf333a0b6c1aaf82b324e2aba13ab290
Haley Moses <vhr825@gmail.com>
2022-05-24 06:11:39 -0500

    switched to yarn and updated a bunch of internal settings

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,26 +1,15 @@
 import * as React from 'react';
-import { useIdentityContext } from 'react-netlify-identity-widget'
-
 
 
 
 export default function Login() {
 
- 
-    const IdentityModal = React.lazy(() => import("react-netlify-identity-widget"))
-    const identity = useIdentityContext()
-    const [dialog, setDialog] = React.useState(false)
-    const isLoggedIn = identity && identity.isLoggedIn
    
-
     return (
-    <div className='loginContainer'>
-        <button className="btn" onClick={() => setDialog(isLoggedIn)}>
-            {isLoggedIn ? "LOG OUT" : "LOG IN"}
-        </button>
-        <React.Suspense fallback="loading...">
-            <IdentityModal showDialog={dialog} onCloseDialog={() => setDialog(false)} />
-        </React.Suspense>
-    </div>
-        );
-    }
+        <div className="loginContainer">
+           
+        </div>   
+    )
+}
+
+      
--- src/index.js
+++ src/index.js
@@ -2,20 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
-import { IdentityContextProvider } from "react-netlify-identity-widget"
-
-
-const url = "https://jocular-madeleine-0bc72c.netlify.app/"
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-    <IdentityContextProvider value={url}>
         <BrowserRouter>
               <App />
         </BrowserRouter>
-    </IdentityContextProvider>
 
 );
 
